92d2ab0addf8d40d2965e5bd74890e46b81f1dd4
FACUNDO GIRAUDI RIVERÓS <47699175@ort>
2023-09-20 11:20:13 -0300

    prueba

---
+++ Models/Alumnos.cs
@@ -0,0 +1,13 @@
+namespace Prac.Models
+{
+    public class Alumnos
+    {
+        public int id { get;set;}
+        public string nombre { get;set;}
+        public string apellido {get;set;}
+        public string curso {get;set;}
+        public int edad {get;set;}
+        public string legajo {get;set;}
+    }
+    
+}
---
+++ Models/BD.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+namespace Prac.Models
+{
+    public static class BD
+    {
+        private static string _connectionString = @"Server=localhost\SQLEXPRESS;Database=GestionAlumnos;Integrated Security=True;";
+        
+        public static List<Alumnos> SeleccionarAlumnos()
+        {
+            List<Alumnos> ListaAlumnos;
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Alumnos";
+                ListaAlumnos = db.Query<Alumnos>(sql).ToList();
+            }
+            return ListaAlumnos;
+        }
+    
+        public static Alumnos AlumnoElegido(int id)
+        {
+            Alumnos Elegido = null;
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string SQL = "SELECT * FROM Alumnos WHERE id= id";
+                Elegido = db.QueryFirstOrDefault<Alumnos>(SQL, new { Alumnos= id });
+            }
+            return Elegido;
+        }
+
+     
